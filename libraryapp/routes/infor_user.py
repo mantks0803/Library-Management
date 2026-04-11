f151582c720290db9fa2e6b7efafd4652ef98539
man_tks <dephucau@gmail.com>
2026-04-11 16:08:46 +0700

    Thêm 1 số trang: cart,borrow history,....

--- libraryapp/routes/infor_user.py
+++ libraryapp/routes/infor_user.py
@@ -24,3 +24,39 @@ def render_user():
 def render_edit_infor():
     user = get_current_user(current_user.id)
     return render_template("auth/update_infor_user.html", user=user)
+
+
+
+@infor_user_bp.route('/history', methods=['GET'])
+@permission()  #
+def render_history():
+#test thử
+
+    history_data = [
+        {
+            "id": "PM001",
+            "book_title": "Frieren: Beyond Journey's End",
+            "borrow_date": "01/04/2026",
+            "due_date": "08/04/2026",
+            "return_date": "05/04/2026",
+            "status": "Đã trả"
+        },
+        {
+            "id": "PM002",
+            "book_title": "Lập trình Python và Flask",
+            "borrow_date": "10/04/2026",
+            "due_date": "17/04/2026",
+            "return_date": None,
+            "status": "Đang mượn"
+        },
+        {
+            "id": "PM003",
+            "book_title": "Cấu trúc dữ liệu và Giải thuật",
+            "borrow_date": "01/03/2026",
+            "due_date": "08/03/2026",
+            "return_date": None,
+            "status": "Quá hạn"
+        }
+    ]
+
+    return render_template("reader/history.html", history_list=history_data)
